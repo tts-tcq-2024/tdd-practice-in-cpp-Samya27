77f1c7e57b97f729e1da83f24759a66f0f8ca5ef
Samya Sharma <68061663+Samya27@users.noreply.github.com>
2024-07-11 02:44:28 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -35,12 +35,14 @@ std::string ReplaceWithCommas(const std::string& input, const std::string& delim
 }
 
 
-int FindSum(std::stringstream& ss){
+int FindSum(const std::string& input){
+    // Creating a stringstream
+    std::stringstream strstream(updatedinput);
     std::string segment;
     int sum = 0;
     std::vector<int> negatives;
 
-    while (std::getline(ss, segment, ',')) {
+    while (std::getline(strstream, segment, ',')) {
         int number = std::stoi(segment);
         if (number < 0) {
             throw std::runtime_error("negatives not allowed");
@@ -71,11 +73,8 @@ int StringCalculator::add(const std::string& input)
     // Replace delimiter and newlines with commas for easy splitting
     std::string updatedinput= ReplaceWithCommas(input,delimiter)
 
-    // Creating a stringstream
-    std::stringstream ss(updatedinput);
-
-    sum = FindSum(ss);
-
+    sum = FindSum(updatedinput);
+    
     //FindNegatives(negatives);
 
     return sum;
